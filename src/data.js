1b4f6a5ed61c7f112498efa171c58f8bb87c5727
Stepiiis <beranstepan@icloud.com>
2022-07-21 22:11:21 +0200

    projects edit

---
+++ src/data.js
@@ -0,0 +1,18 @@
+export const projects = [
+    {
+        title: "React TODO",
+        subtitle: "React Hooks, Bootstrap",
+        description:
+            "Simple app to demonstratate understanding of React fundamentals",
+        image: "./todo_app_demo.png",
+        link: "https://github.com/Stepiiis/react_kurz/tree/master/projekt_todo",
+    },
+    {
+        title: "Tower Attack",
+        subtitle: "C++, ncurses",
+        description:
+            "Simple Tower Attack game, made in c++ using the ncurses library. Final project of the C++ course.",
+        image: "./power_attack.png",
+        link: "https://github.com/Stepiiis/pa2_powerAttack",
+    },
+    ];
